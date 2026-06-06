3c181b0a6e43fde01705d35c9233d09bf1ee4f6f
Naga Rani <22nn1a0551nagarani@gmail.com>
2026-06-06 21:19:12 +0530

    Initial commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\m4827\OneDrive\Desktop\krushi_portal\venv
+set VIRTUAL_ENV=C:\Users\NAGARANI\OneDrive\Desktop\sih\SIH\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
